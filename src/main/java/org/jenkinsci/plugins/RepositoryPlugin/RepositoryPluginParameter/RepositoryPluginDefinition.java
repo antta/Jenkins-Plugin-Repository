275f2f62aa221474971985fe428e66e7e1450843
Klyd <anthony.tang@etu.univ-savoie.fr>
2014-04-23 11:05:21 +0200

    23-04-2014 11h05

--- src/main/java/org/jenkinsci/plugins/RepositoryPlugin/RepositoryPluginParameter/RepositoryPluginDefinition.java
+++ src/main/java/org/jenkinsci/plugins/RepositoryPlugin/RepositoryPluginParameter/RepositoryPluginDefinition.java
@@ -1,10 +1,8 @@
 package org.jenkinsci.plugins.RepositoryPlugin.RepositoryPluginParameter;
 
-import fr.univsavoie.serveurbeta.trap.Trap;
 import hudson.Extension;
 import hudson.model.ParameterValue;
 import hudson.model.ParameterDefinition;
-import hudson.util.FormValidation;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -15,27 +13,18 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 import org.apache.commons.lang.StringUtils;
-import org.dom4j.DocumentException;
 import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
 public class RepositoryPluginDefinition extends ParameterDefinition {
 	private static final long serialVersionUID = 750881539978939359L;
-	private String repoAlias;
-	private String url;
 	private String multiSelectDelimiter;
-	private int visibleItemCount;
 	private String packages;
 	
 	@DataBoundConstructor
-	public RepositoryPluginDefinition(String name, String description, String repoAlias,
-			String url, int visibleItemCount) {
+	public RepositoryPluginDefinition(String name, String description) {
 		super(name, description);
-		this.repoAlias = repoAlias;
-		this.url = url;
 		this.multiSelectDelimiter = ",";
-		this.visibleItemCount = (visibleItemCount == 0 ? 5 : visibleItemCount);
 	}
 
 	@Override
@@ -87,15 +76,6 @@ public class RepositoryPluginDefinition extends ParameterDefinition {
 		public String getDisplayName() {
 			return "Repository Parameter";
 		}
-
-		public FormValidation doCheckUrl(@QueryParameter String url) throws DocumentException {
-			Trap trap = new Trap("/var/lib/jenkins/jobs/Job Creation Repository/root/");
-			if (!trap.isAValidRepository(url)) {
-				return FormValidation.error("Repository URL is invalid !");
-			}
-			return FormValidation.ok("Valid URL");
-		}
-
 	}
 	
 	public String getMultiSelectDelimiter() {
@@ -109,40 +89,12 @@ public class RepositoryPluginDefinition extends ParameterDefinition {
 	public String getPackages() {
 		return packages;
 	}
-
-	public void setValue(String packages) {
-		this.packages = packages;
-	}
 	
-	public int getVisibleItemCount() {
-		return visibleItemCount;
-	}
-
-	public void setVisibleItemCount(int visibleItemCount) {
-		this.visibleItemCount = visibleItemCount;
-	}
-	
-	public String getRepoAlias() {
-		return repoAlias;
-	}
-
-	public void setRepoAlias(String repoAlias) {
-		this.repoAlias = repoAlias;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
 	public void setPackages(String packages) {
 		this.packages = packages;
 	}
 
 	public void getString() {
-		setValue("myjenkins, mysonar, mynexus");
+		setPackages("myjenkins, mysonar, mynexus");
 	}
 }
--- src/main/java/org/jenkinsci/plugins/RepositoryPlugin/RepositoryPluginWrapper/RepositoryPluginWrapper.java
+++ src/main/java/org/jenkinsci/plugins/RepositoryPlugin/RepositoryPluginWrapper/RepositoryPluginWrapper.java
@@ -1,5 +1,6 @@
 package org.jenkinsci.plugins.RepositoryPlugin.RepositoryPluginWrapper;
 
+import fr.univsavoie.serveurbeta.trap.Trap;
 import hudson.Extension;
 import hudson.Launcher;
 import hudson.model.BuildListener;
@@ -7,26 +8,59 @@ import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.tasks.BuildWrapper;
 import hudson.tasks.BuildWrapperDescriptor;
+import hudson.util.FormValidation;
 
 import java.io.IOException;
 
+import org.dom4j.DocumentException;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 
 public class RepositoryPluginWrapper extends BuildWrapper {
+	private String repoAlias;
+	private String url;
+	private int visibleItemCount;
 	private static String absolutePath;
 	
 	@DataBoundConstructor
-	public RepositoryPluginWrapper() {
+	public RepositoryPluginWrapper(String name, String description, String repoAlias,
+			String url, int visibleItemCount) {
+		this.repoAlias = repoAlias;
+		this.url = url;
+		this.visibleItemCount = (visibleItemCount == 0 ? 5 : visibleItemCount);
 	}
 	
+	public String getRepoAlias() {
+		return repoAlias;
+	}
+
+	public void setRepoAlias(String repoAlias) {
+		this.repoAlias = repoAlias;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public int getVisibleItemCount() {
+		return visibleItemCount;
+	}
+
+	public void setVisibleItemCount(int visibleItemCount) {
+		this.visibleItemCount = visibleItemCount;
+	}
+
 	@Extension
  	public static final class DescriptorImpl extends BuildWrapperDescriptor {
-		
 		@Override
 		public String getDisplayName() {
 			return "Get job absolute path";
 		}
-
+		
 		@Override
 		public boolean isApplicable(AbstractProject<?, ?> project) {
 			try {
@@ -36,6 +70,16 @@ public class RepositoryPluginWrapper extends BuildWrapper {
 				e.printStackTrace();
 			}
 			return true;
+		}
+		
+		public FormValidation doCheckUrl(@QueryParameter String url) throws DocumentException {
+			//*
+			Trap trap = new Trap("/var/lib/jenkins/jobs/Job Creation Repository/root/");
+			if (!trap.isAValidRepository(url)) {
+				return FormValidation.error("Repository URL is invalid !");
+			}
+			//*/
+			return FormValidation.ok("Valid URL");
 		}
 	}
 	
