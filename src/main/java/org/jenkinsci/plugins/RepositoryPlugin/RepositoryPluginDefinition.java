afc016aac6e6aca80b8ac16405abe0a0a9082ad7
cyril <m@m.fr>
2014-04-30 11:33:27 +0200

    Commit du plugin de Gregoire au 30-04-2014

--- src/main/java/org/jenkinsci/plugins/RepositoryPlugin/RepositoryPluginDefinition.java
+++ src/main/java/org/jenkinsci/plugins/RepositoryPlugin/RepositoryPluginDefinition.java
@@ -5,33 +5,60 @@ import hudson.Extension;
 import hudson.model.ParameterValue;
 import hudson.model.ParameterDefinition;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import hudson.util.FormValidation;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 import org.apache.commons.lang.StringUtils;
+import org.dom4j.DocumentException;
 import org.jenkinsci.plugins.RepositoryPlugin.RepositoryPluginWrapper;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
+
+import javax.swing.*;
 
 public class RepositoryPluginDefinition extends ParameterDefinition {
     private static int nbInstance = 0;
 	private static final long serialVersionUID = 750881539978939359L;
-	private String multiSelectDelimiter;
+    private final String repoAlias;
+    private final String url;
+    private final String path;
+    private String multiSelectDelimiter;
     private String packages;
     private int idInstance = nbInstance++;
 	private int visibleItemCount;
+
+
+    private static Trap trap;
 	
 	@DataBoundConstructor
-	public RepositoryPluginDefinition(String name, String description) {
-		super(name, description);
+	public RepositoryPluginDefinition(String name, String description, String repoAlias, String url, int visibleItemCount) {
+		super(repoAlias, url);
 		this.multiSelectDelimiter = ",";
-        SharedData.addRepoDefinition(this.idInstance, this);
+        this.repoAlias = repoAlias;
+        this.visibleItemCount = (visibleItemCount < 0 ? 5 : visibleItemCount);
+        this.url = url;
+        this.path = RepositoryPluginWrapper.getLastPath() + "/zypproot";
+
+
+        File folder = new File(path);
+        trap = new Trap(path,false);
+        folder.mkdir();
+
+        trap.addRepository(repoAlias, url);
+        trap.refreshRepo(repoAlias);
 	}
+
 
 	@Override
 	public ParameterValue createValue(StaplerRequest request) {
@@ -39,6 +66,7 @@ public class RepositoryPluginDefinition extends ParameterDefinition {
 		if(requestValues == null || requestValues.length == 0) {
 			return getDefaultParameterValue();
 		}
+
 		String valueStr = getPackages();
 		if(valueStr != null) {
 			List<String> result = new ArrayList<String>();
@@ -89,6 +117,18 @@ public class RepositoryPluginDefinition extends ParameterDefinition {
 		public String getDisplayName() {
 			return "Repository Parameter";
 		}
+
+        public FormValidation doCheckUrl(@QueryParameter String url) throws DocumentException, IOException {
+
+            //*
+            RepositoryPluginDefinition.trap = RepositoryPluginDefinition.getTrap();
+
+            if (!trap.isAValidRepository(url)) {
+                return FormValidation.error("Repository URL is invalid !");
+            }
+            //*/
+            return FormValidation.ok();
+        }
 	}
 	
 	public String getPackages() {
@@ -100,20 +140,36 @@ public class RepositoryPluginDefinition extends ParameterDefinition {
     public void setPackages(String packages) {
         this.packages = packages;
     }
+
     public void getPackageList() {
-        String repo = SharedData.getRepoWrapper(this.idInstance).getRepoAlias();
-        Trap trap = RepositoryPluginWrapper.getTrap();
-        trap.refreshRepo(repo);
-        setVisibleItemCount(SharedData.getRepoDefinition(this.idInstance).visibleItemCount);
-        setPackages(trap.getPackagesIn(repo));
+        Trap trap = new Trap(path,false);
+        trap.refreshRepo(repoAlias);
+        File file = new File(path+"log.txt");
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.write(System.currentTimeMillis() + " getPackages list ! repoAlias : " + repoAlias + " url : " + url + " path : " + path + " packages : " + packages);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setPackages(trap.getPackagesIn(repoAlias));
     }
 
 	public int getVisibleItemCount() {
         return visibleItemCount;
 	}
 
-	public void setVisibleItemCount(int visibleItemCount) {
-		this.visibleItemCount = visibleItemCount;
-	}
+    public String getRepoAlias() {
+        return repoAlias;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public static Trap getTrap() {
+        if (trap == null)
+            trap = new Trap("tmpZyppRoot/",false);
+        return trap;
+    }
 
 }
--- src/main/java/org/jenkinsci/plugins/RepositoryPlugin/RepositoryPluginWrapper.java
+++ src/main/java/org/jenkinsci/plugins/RepositoryPlugin/RepositoryPluginWrapper.java
@@ -18,57 +18,33 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
 public class RepositoryPluginWrapper extends BuildWrapper {
-    private static int nbInstance = 0;
-	private String repoAlias;
-	private String url;
-    private int idInstance = nbInstance++;
-	public int visibleItemCount;
-	public String packageList;
-    private static Trap trap;
+    private static String lastPath;
 
 	@DataBoundConstructor
-	public RepositoryPluginWrapper(String repoAlias, String url, int visibleItemCount) {
-		this.repoAlias = repoAlias;
-		this.url = url;
-		this.visibleItemCount = (visibleItemCount == 0 ? 5 : visibleItemCount);
-        SharedData.addRepoWrapper(this.idInstance, this);
-
-		trap.addRepository(repoAlias, url);
-        trap.refreshRepo(repoAlias);
-		//RepositoryPluginWrapper.packageList = trap.getPackagesIn(repoAlias);
-
+	public RepositoryPluginWrapper() {
 	}
-	
+
 	@Extension
  	public static final class DescriptorImpl extends BuildWrapperDescriptor {
 		private AbstractProject<?, ?> project;
-		
+
 		@Override
 		public String getDisplayName() {
-			return "Add repository URL";
+			return "Set static project path";
 		}
-		
+
 		@Override
 		public boolean isApplicable(AbstractProject<?, ?> project) {
 			this.project = project;
-			return true;
-		}
-		
-		public FormValidation doCheckUrl(@QueryParameter String url) throws DocumentException, IOException {
-			String absolutePath = project.getRootDir().getCanonicalPath();
-			//*
-            RepositoryPluginWrapper.trap = RepositoryPluginWrapper.getTrap();
-
-			if (!trap.isAValidRepository(url)) {
-				return FormValidation.error("Repository URL is invalid !");
-			}
-			//*/
-			File folder = new File(absolutePath + "/zypproot");
-		    folder.mkdir();
-			return FormValidation.ok();
+            try {
+                lastPath = project.getRootDir().getCanonicalPath();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return true;
 		}
 	}
-	
+
 	@SuppressWarnings("rawtypes")
 	public Environment setUp(AbstractBuild build, Launcher launcher, BuildListener listener)
 			throws IOException, InterruptedException {
@@ -81,34 +57,7 @@ public class RepositoryPluginWrapper extends BuildWrapper {
 		};
 	}
 
-	public String getRepoAlias() {
-		return repoAlias;
-	}
-
-	public void setRepoAlias(String repoAlias) {
-		this.repoAlias = repoAlias;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public int getVisibleItemCount() {
-		return visibleItemCount;
-	}
-
-	public void setVisibleItemCount(int visibleItemCount) {
-		this.visibleItemCount = visibleItemCount;
-	}
-
-    public static Trap getTrap() {
-        if (trap == null) {
-            trap = new Trap("root/", false);
-        }
-        return trap;
+    public static String getLastPath() {
+        return lastPath;
     }
 }
